d52a58ec6715ba7c16a1d42d5f904a6820196dcf
bjspeaks <20682046+bjspeaks@users.noreply.github.com>
2021-02-11 22:30:59 +0000

    Update AtlasAPI-Test.py

--- AtlasAPI-Test.py
+++ AtlasAPI-Test.py
@@ -14,11 +14,11 @@ https://atlas.apache.org/api/v2/ui/index.html
 '''
 url = "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/token"
 client_id = "23443326-7c07-4c96-a34c-0d84bc9aa2b2"
-client_secret = "9e.GH~jL5Z1~.dRW8A8M67uPo-0Ps_16u~"
+client_secret = ""
 
 myobj = {'grant_type': 'client_credentials',
-         'client_id' : '23443326-7c07-4c96-a34c-0d84bc9aa2b2',
-         'client_secret':'9e.GH~jL5Z1~.dRW8A8M67uPo-0Ps_16u~',
+         'client_id' : '',
+         'client_secret':'',
          'resource':'73c2949e-da2d-457a-9607-fcc665198967'
         }
 
